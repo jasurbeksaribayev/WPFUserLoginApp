aafbedec0f3f7140260112bd236bbe8b88930f47
Victus <jasurbeksaribayevjs0000@gmail.com>
2023-08-02 00:33:13 +0500

    Initial commit1

--- WpfApp1/obj/Debug/net6.0-windows/Pages/MarketAddPage.g.i.cs
+++ WpfApp1/obj/Debug/net6.0-windows/Pages/MarketAddPage.g.i.cs
@@ -73,7 +73,7 @@ namespace WpfApp1.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/pages/marketaddpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/pages/marketaddpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\MarketAddPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
