ba697cd49f53411e0f6ca0e774729bce354539a0
Victus <jasurbeksaribayevjs0000@gmail.com>
2023-08-02 12:13:24 +0500

    Initial commit6

--- WpfApp1.Data/DbContexts/WpfApp1DbContext.cs
+++ WpfApp1.Data/DbContexts/WpfApp1DbContext.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WpfApp1.Domain.Entities;
 
 namespace WpfApp1.Data.DbContexts
@@ -14,7 +9,7 @@ namespace WpfApp1.Data.DbContexts
         public DbSet<Market> Markets { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "server=localhost;port=3306;database=mydatabase;user=root;password=";
+            string connectionString = "server=localhost;port=3306;database=mydatabase;user=root;password=Coderfrom2022";
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
     }
